ad377f07cbfe49a02355e959608579871e594640
Piyush Singh <workspace.piyush01@gmail.com>
2026-01-07 21:26:14 +0530

    Removed User repo and mongo conf

--- src/main/java/com/reco/emailservice/model/User.java
+++ src/main/java/com/reco/emailservice/model/User.java
@@ -1,12 +1,7 @@
 package com.reco.emailservice.model;
 
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-@Document(collection = "users")
 public class User {
 
-	@Id
 	private String id;
 	private String email;
 	private String name;
